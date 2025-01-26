7afec55395e14721ae7cc2e30b5944e08ccc8cd4
standard-t <147738136+standard-t@users.noreply.github.com>
2025-01-26 12:25:04 -0500

    projects section made

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div id='contact' className='flex min-h-screen w-full flex-col items-center justify-center gap-16 p-8 '>
+            <h1 className=''>Get in Touch</h1>
+        </div>
     )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,99 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <div id='projects' className='min-h-screen w-full flex flex-col gap-20 p-10 md:p-16 xl:p-32 '>
+            <h1 className='text-center text-5xl font-light'>Projects</h1>
+            <h1 className='text-align-left text-3xl font-light'>Design</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+            </div>
+            <h1 className='text-align-left text-3xl font-light'>Coding</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-[#939e7b] rounded-lg hover:bg-[#f3f7eb] ">
+                    <h1 className='text-3xl font-semibold '>Project1</h1>
+                    <h3 className='text-xl text-gray-700'>Techstack</h3>
+                    <p className='text-sm text-gray-600'>
+                        I’m passionate about combining my technical skills with my creative side to design user-friendly and intuitive digital experiences. Right now, I’m diving into courses and projects that help me learn more about user research, wireframing, prototyping, data visualization, and web development.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
