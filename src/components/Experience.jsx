284f9b13e57e6f08dcca0e5ecd99a527f206bd06
standard-t <147738136+standard-t@users.noreply.github.com>
2025-01-26 16:55:02 -0500

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,46 +11,46 @@ const Experience = () => {
 
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 1</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>SkiMate</h3>
+                        <p className='font-light text-lg text-gray-600'>Lead Product Designer</p>
+                        <p className='text-sm text-gray-600'>Spearheaded the design of a mobile app to connect snow sports enthusiasts, managing the mobile interface, workflows, brand identity, and design system. Collaborated with the CEO, product manager, and developers to define the target audience and value proposition, setting the stage for a successful Phase 1 launch. Developed user requirements and scenarios to ensure a seamless, engaging experience.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 2</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>Northeastern Project Hot Sauce</h3>
+                        <p className='font-light text-lg text-gray-600'>Student User Experience Designer</p>
+                        <p className='text-sm text-gray-600'>Partnered with Northeastern Campus Operations and sensor engineers to design a data visualization system displaying live and historical HVAC system status (temperature, humidity, CO2) to identify trends and optimize functionality. Conducted user research and testing through interviews and observational studies to identify needs, create interaction scenarios, and inform design decisions, ultimately delivering a polished Figma prototype.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 3</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>NuSci Magazine</h3>
+                        <p className='font-light text-lg text-gray-600'>Magazine Design</p>
+                        <p className='text-sm text-gray-600'>Designed article pages and spreads for the print edition of a student-run science magazine, focusing on visually engaging layouts that complemented the content and enhanced readability. Worked closely with editors and writers to ensure each design aligned with the magazine’s vision and communicated complex scientific concepts in an accessible and visually appealing way. Contributed to the overall look and feel of the publication, helping to maintain a cohesive design language throughout.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                 </div>
                 <div className="flex1">
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 4</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>Mass General Brigham Safe Sleep Initiative</h3>
+                        <p className='font-light text-lg text-gray-600'>Visual Designer</p>
+                        <p className='text-sm text-gray-600'>Crafted a suite of posters, brochures, and promotional materials to educate new parents on safe sleeping practices for newborns, displayed throughout Mass General’s NICU and maternity ward. Simplified complex information to highlight key points, dispel misconceptions, and reduce infant sleep-related deaths through clear, accessible design, all while adhering to Mass General’s style guidelines.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 5</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>Needham Craft Fair</h3>
+                        <p className='font-light text-lg text-gray-600'>Advertisement Designer</p>
+                        <p className='text-sm text-gray-600'>Collaborated with event administration to develop and execute social media campaigns promoting a local craft fair. Created eye-catching advertisements tailored to different platforms, aimed at increasing event visibility and engagement within the community. Worked closely with the team to align messaging with the fair’s branding and target audience, driving attendance and excitement.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                     <div className="relative space-y-1 border-l-2 p-8 text-left">
 
-                        <h3 className='text-xl font-semibold md:text-2xl'>Experience</h3>
-                        <p className='font-light text-lg text-gray-600'>Job Title</p>
-                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <h3 className='text-xl font-semibold md:text-2xl'>Sunday River</h3>
+                        <p className='font-light text-lg text-gray-600'>Ski Instructor</p>
+                        <p className='text-sm text-gray-600'>Organized and taught ski lessons to groups of up to 9 children, ages 3-4, creating an engaging and educational environment that kept young learners excited and motivated. Focused on balancing skill development with fun, while also assessing and addressing each child’s physical and emotional needs to ensure a positive and supportive experience.</p>
                         <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
                     </div>
                 </div>
