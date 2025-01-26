7e40e5f7935023d624b56795d9a7792741be1546
standard-t <147738136+standard-t@users.noreply.github.com>
2025-01-26 11:55:30 -0500

    experience section made

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,60 @@ import React from 'react'
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <div id='experience' className='flex min-h-screen w-full flex-col items-center justify-center gap-20  p-14 md:p-20 lg:p-36'>
+            <h1 className='text-center text-5xl font-light'>Experience</h1>
+
+            <div className="flex flex-wrap gap-0 md:flex-nowrap md:gap-10">
+
+                <div className="flex1">
+
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 1</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 2</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 3</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                </div>
+                <div className="flex1">
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 4</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 5</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                    <div className="relative space-y-1 border-l-2 p-8 text-left">
+
+                        <h3 className='text-xl font-semibold md:text-2xl'>Experience 6</h3>
+                        <p className='font-light text-lg text-gray-600'>Job Title</p>
+                        <p className='text-sm text-gray-600'>Hi there! I'm an aspiring UX/UI designer working towards my bachelor’s degree in Computer Science and Design at Northeastern University. Job Description. </p>
+                        <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-[#939e7b]'></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
