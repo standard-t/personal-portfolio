58e20ed37493432d5409324b04e2b3d67a1c916c
standard-t <147738136+standard-t@users.noreply.github.com>
2025-02-26 15:07:50 -0500

    added canvas project

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -1,5 +1,24 @@
-
 import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive1 = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        slidesToSlide: 1
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1,
+        slidesToSlide: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1
+    }
+};
 
 const Coding = () => {
     return (
@@ -48,6 +67,22 @@ const Coding = () => {
                         <h1 className='text-3xl font-semibold py-2'>Typing Game</h1>
                         <p className='max-w-[350px] text-sm text-gray-600'>
                             Developed the core mechanics of a falling-strings typing game in Java, which generates random strings and animates their descent at a specified rate, captures user input, and validates with key even listeners. When player types a string, its color changes as a visual indicator, and input is restricted to that string until completed. This project enhanced my ability to work with game loops, manage real-time input, and implement efficient game logic in a dynamic environment.
+                        </p>
+                    </div>
+                </div>
+                <div className="text-left justify-center space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-12 py-10 border-[#939e7b] rounded-lg ">
+                    <Carousel responsive={responsive1} infinite={true} centerMode={true}>
+                        <div className='px-3'><img src="./canvas/canvas1.png" /></div>
+                        <div className='px-3'><img src="./canvas/canvas2.png" /></div>
+                        <div className='px-3'><img src="./canvas/canvas3.png" /></div>
+                        <div className='px-3'><img src="./canvas/canvas4.png" /></div>
+                        <div className='px-3'><img src="./canvas/canvas5.png" /></div>
+                        <div className='px-3'><img src="./canvas/canvas6.png" /></div>
+                    </Carousel>
+                    <div className='px-5'>
+                        <h1 className='text-3xl font-semibold pt-5 pb-2'>Canvas Replica</h1>
+                        <p className='text-sm text-gray-600 max-w-[650px]'>
+                            Recreating a web-based Learning Management System (LMS) similar to Canvas, using HTML, CSS, and JavaScript to replicate the platform’s aesthetic and functionality with the goal of creating a responsive and user-friendly interface that dynamically loads course materials, assignments, grades, and people. Used HTML and CSS to design a responsive layout, while utilizing JavaScript to ensure smooth, dynamic content rendering and manage user interactions. Currently working on integrating RESTful APIs and NoSQL for further functionality and improved data management.
                         </p>
                     </div>
                 </div>
